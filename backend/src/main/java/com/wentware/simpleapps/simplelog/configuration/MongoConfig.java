41afd45ba431d7047f9b93e5f2aec3748aa33ea0
Ed Wentworth <edlovesjava@yahoo.com>
2014-11-26 15:36:40 -0500

    configured project to serve sencha touch default app along with spring data rest serving from mongodb

---
+++ backend/src/main/java/com/wentware/simpleapps/simplelog/configuration/MongoConfig.java
@@ -0,0 +1,51 @@
+package com.wentware.simpleapps.simplelog.configuration;
+
+import com.mongodb.Mongo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+/**
+ *
+ * @author edlovesjava
+ */
+@Configuration
+@ConfigurationProperties
+public class MongoConfig {
+
+    @Value("${spring.datasource.user.name}")
+    private String dbUserName;
+    @Value("${spring.datasource.user.password}")
+    private String dbPassword;
+    @Value("${spring.datasource.connection.url}")
+    private String connectionUrl;
+    @Value("${spring.datasource.usecredentials}")
+    private String useCredentials;
+    @Value("${spring.datasource.database}")
+    private String database;
+    
+    public @Bean
+    MongoDbFactory mongoDbFactory() throws Exception {
+        UserCredentials userCredentials = new UserCredentials(dbUserName, dbPassword);
+
+        if ("TRUE".equals(useCredentials.toUpperCase())) {
+            System.out.println(">>> using credentials '"+useCredentials+"'");
+            return new SimpleMongoDbFactory(new Mongo(connectionUrl), database, userCredentials);
+        } else {
+            System.out.println(">>> NOT using credentials '"+useCredentials+"'");
+            return new SimpleMongoDbFactory(new Mongo(connectionUrl), database);
+            
+        }
+        //return new SimpleMongoDbFactory(new Mongo(), "database");
+    }
+
+    public @Bean
+    MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongoDbFactory());
+    }
+}
---
+++ backend/src/main/java/com/wentware/simpleapps/simplelog/documents/Person.java
@@ -0,0 +1,60 @@
+package com.wentware.simpleapps.simplelog.documents;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+
+    @Id
+    private String id;
+
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+        
+    }
+    
+    public Person(String firstName, int age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+}
