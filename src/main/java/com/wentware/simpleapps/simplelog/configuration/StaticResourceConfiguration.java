43be61fbc5bb3391a41506e9a3e318bb48a2f1c3
Ed Wentworth <edlovesjava@yahoo.com>
2014-12-11 22:50:01 -0500

    set correct configuration of static resource path to static

---
+++ src/main/java/com/wentware/simpleapps/simplelog/configuration/StaticResourceConfiguration.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wentware.simpleapps.simplelog.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/static/");
+    }
+}
